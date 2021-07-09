ef10de85f6e5d3bf1228a81ecb232bd67da2a3da
Hubert Bukowski <58427920+hubo00@users.noreply.github.com>
2021-07-09 09:10:34 +0100

    uploaded updated numguesser

--- game.js
+++ game.js
@@ -15,14 +15,16 @@ function displayGuess(){
 
 function lower(){
     max = compGuess - 1;
-    console.log(compGuess + " is the new maximum")
+    document.getElementById("log").innerHTML += ("IF lower THEN set new max value to old guess. New Max: " + max + "<br>")
+    document.getElementById("Max").innerHTML = max
     attempts++;
     displayGuess();
 }
 
 function higher(){
     min = compGuess + 1;
-    console.log(compGuess + " is the new minimum")
+    document.getElementById("log").innerHTML += ("IF higher THEN set new min value to old guess. New Min: " + min + "<br>")
+    document.getElementById("Min").innerHTML = min
     attempts++;
     displayGuess();
 }
