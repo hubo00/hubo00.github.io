a14b6fed782e7352bcb95f1e641c6126991a784a
Hubert Bukowski <58427920+hubo00@users.noreply.github.com>
2021-07-09 09:13:56 +0100

    Update game.js

--- game.js
+++ game.js
@@ -15,20 +15,20 @@ function displayGuess(){
 
 function lower(){
     max = compGuess - 1;
-    document.getElementById("log").innerHTML += ("IF lower THEN set new max value to old guess. New Max: " + max + "<br>")
-    document.getElementById("Max").innerHTML = max
+    document.getElementById("log").innerHTML += ("IF lower THEN set new max value to old guess. New Max: " + max + "<br>");
+    document.getElementById("Max").innerHTML = max;
     attempts++;
     displayGuess();
 }
 
 function higher(){
     min = compGuess + 1;
-    document.getElementById("log").innerHTML += ("IF higher THEN set new min value to old guess. New Min: " + min + "<br>")
-    document.getElementById("Min").innerHTML = min
+    document.getElementById("log").innerHTML += ("IF higher THEN set new min value to old guess. New Min: " + min + "<br>");
+    document.getElementById("Min").innerHTML = min;
     attempts++;
     displayGuess();
 }
 
 function correct(){
-    document.getElementById("correct").innerHTML = "Yay! I managed to get it in " + attempts + " tries!"
+    document.getElementById("correct").innerHTML = "Yay! I managed to get it in " + attempts + " tries!";
 }
